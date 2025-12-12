6160bfcec158a1894c2295a414a97a1f88048cf7
AYOUB <ayoub.ardem@gmail.com>
2025-12-12 21:14:59 +0100

    Update schema.py

--- alx_backend_graphql_crm/alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/alx_backend_graphql_crm/schema.py
@@ -1,11 +1,14 @@
 import graphene
-from .models import Customer,Order,Product
-import re
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
 class Query(graphene.ObjectType):
   hello = graphene.String(default_value="Hello, GraphQL!")
 
 
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
 
-schema=graphene.Schema(query=Query,mutation=Mutation)
+schema = graphene.Schema(query=Query, mutation=Mutation)
